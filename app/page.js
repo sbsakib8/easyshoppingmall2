77d4b55197b465925ada99c1521383db3e76e460
sakib <sbsakibsarkar8@gmail.com>
2025-07-06 05:31:32 -0700

    start e commerce project

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Topheader from "@/src/compronent/header/Topheader";
+
+export default function Home() {
+  return (
+    
+    <div>
+     <Topheader/>
+    </div>
+  );
+}
---
+++ src/compronent/header/Leftheader.jsx
@@ -0,0 +1,8 @@
+
+function Leftheader() {
+  return (
+    <div>Leftheader</div>
+  )
+}
+
+export default Leftheader
